64c07730b09cc70ad2441cfc8087bffe2a583b99
clavell <clavell@users.noreply.github.com>
2019-11-23 23:15:18 -0800

    project finished - many features added

--- script/script.js
+++ script/script.js
@@ -1,17 +1,59 @@
+var arTitles = ["game", "review", "about","random"];
+var navLinks = ["embedded_game.html",
+                "review.html",
+                "index.html#about",
+                "https://en.wikipedia.org/wiki/Special:RandomInCategory/Steampunk_video_games"]; 
+
 var imagePaths = ["/images/tiger-pixabay.jpg",
                 "/images/hot-air-balloon-pixabay.png",
                 "https://d2fzf9bbqh0om5.cloudfront.net/images/5019/blog_main/the_8_best_steampunk_games_you_should_be_playing.jpg?1548054878",
-            ]
+            ];
+
+var thumbTitles = ["Steampunk Adventure",
+                    "hot air balloon warz",
+                    "Fantasy time space man",
+                    "clock watcher 64",                    
+                    "program the AE (it's hard)",
+                    "chess",
+                  ];
+
+var thumbImages = ["images/steampunk_adventure.jpg",
+                  "images/hot-air-balloon-pixabay.png",
+                  "images/fantasy_time_space_man.jpg",
+                  "images/clock_watcher_64.png",
+                  "images/AnalyticalEngineWiki.jpg",
+                  "images/chess.jpg"];
+
+var thumbLinks = ["https://gameades.itch.io/steampunk-adventure",
+                  "","","","",""];
+
+/************************************************************************************************* */
+//a function to set a ranom picture for the slideshow
+function setMain(){
+  var slides = document.getElementById("slides");
+  var mainImage = document.createElement("img");
+  var whichImage = Math.floor(Math.random()*6)
+  mainImage.src= thumbImages[whichImage];
+  slides.appendChild(mainImage);
+  mainImage.className = "mainImage";
+}
+//end setMain function
+
+
+/********************************************************************************************************** */
 
 //a function written to open the menu used in conjunction with menuClose() when hovering over the menu
 //these did not perform as intended so subbing out in favour of clickMenuOpen() and clickMenuClose()
 //keeping in I figure out a fix
+var open = false;
 function menuOpen(){
   document.getElementById("nav").style.width = "100px";
   document.getElementById("spinner").src = "fidget-spinner/reverse-transparent-bg-fidget-spinner.gif";    
   setTimeout('document.getElementById("nav").style.width = "250px"', 200);    
 }
 //end menuOpen function
+
+/****************************************************************************************************** */
 
 //a function written to close the menu after opened with menuOpen() function when mouse leaves area
 //see menuOpen comments for more info
@@ -22,47 +64,78 @@ function menuClose() {
     
   }
 
+/********************************************************************************************************** */
+
 //function to open menu sidebar when menu icon clicked see menuOpen() function for reason why this function
 //was written
 function clickMenuOpen(){
   var nav = document.getElementById("nav");
-  nav.setAttribute("onclick","clickMenuClose;");
   nav.style.width = "100px";
   document.getElementById("spinner").src = "fidget-spinner/reverse-transparent-bg-fidget-spinner.gif";     
   setTimeout('nav.style.width = "250px"', 200);
-  setTimeout('nav.onclick = clickMenuClose', 700);    
+  setTimeout('nav.onclick = clickMenuClose', 700);  
+  open = true;  
 }
 
+/******************************************************************************************************* */
+//accompanying function to clickMenuOpen()
 function clickMenuClose() {   
   var nav = document.getElementById("nav"); 
-  nav.onclick = "clickMenuOpen();"
   nav.style.width = "200px";
   document.getElementById("spinner").src = "fidget-spinner/transparent-bg-fidget-spinner.gif";
   setTimeout('nav.style.width = "50px"', 200);    
+  open=false;
+}
+//end clickMenuClose function
+
+/********************************************************************************************** */
+//function to decide whether to open or close menu on click
+function clickSpinner(){
+  if(open){
+    clickMenuClose();
   }
+  else{
+    clickMenuOpen();
+  }
+}
+//end clickSpinner function
+
+/************************************************************************************* */
 
 //a function to create the thumbnails in the sidebar (lins with pictures)
-function addThumbnails(elements,images,links,text){
-    for(var x=0; x<elements.length;x++){
+function addThumbnails(images,links,text){
+    for(var x=0; x<images.length;x++){
         var nail = document.createElement('div');
         var image = document.createElement('img');
         var link = document.createElement('a');
-        var text = document.createElement('span');
-        text.innerHTML = text[x];
+        var title = document.createElement('div');
+        title.innerHTML = text[x];
         link.href = links[x];
         image.src = images[x];
+        title.className="thumbTitles";
         image.className="thumbPhotos";
-        link.appendChild(image)
+        link.className="thumbLinks";
+        nail.className="thumbnail";
+        link.appendChild(image);
         nail.appendChild(link);
-        nail.appendChild(text);   
-        document.getElementById('thumbnails').appendChild(nail);
+        link.appendChild(title);   
+        document.getElementById("thumbnails").appendChild(nail);
+        
+        //here we want to make all of the fake links disappear when moused over hahahaha!!
+        if(x!=0){
+          nail.onmouseenter =function(){this.style.opacity = 0;
+                                        this.style.transition = "0.5s"};
+          nail.className=nail.className + " disappear";
+        }
     }
 }//end addThumbnails function
+addThumbnails(thumbImages,thumbLinks,thumbTitles);
 
+/*********************************************************************************************** */
 
 //a function that populates a container with links without pictures
 function createElement(content, containerID, links,color,overColor){
-    console.log(content.length);
+    // console.log(content.length);
     var nav = document.getElementById(containerID);
     //var navBarItems = [];
     for(let x=0;x<content.length;x++){
@@ -80,23 +153,23 @@ function createElement(content, containerID, links,color,overColor){
         
     }    
 }//end createElement function
+createElement(arTitles,"navContent",navLinks,"#222222","#a1a1a1");   
+createElement(arTitles,"header",navLinks,"#f1f1f1","#a1a1a1");
 
+/******************************************************************************************* */
+setMain();
 
-//functions below from W3 schools for moving through slides
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function testArticle(form){
+  var items = document.getElementById("main").children
+  items[0].innerHTML = form.elements.title.value;
+  items[1].innerHTML = "written by " + form.elements.author.value;
+  //split up the review to keep the format of the article more or less the same.
+  var str = form.elements.review.value;
+  var firstHalf = str.slice(0,Math.floor(str.length/2))
+  var secondHalf = str.slice(Math.ceil(str.length/2));
+  items[2].innerHTML = firstHalf;
+  items[4].innerHTML = secondHalf;
+  items[3].outerHTML = form.elements.video.value;
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
-}
+
